63e6b5ed6848b135d6a62039351770560dcef00d
Derrick Wong <142132416+LemonDrew@users.noreply.github.com>
2025-08-29 22:25:05 +0800

    Add 2 basic dashboards/graph layouts

--- src/App.jsx
+++ src/App.jsx
@@ -15,11 +15,6 @@ export function App(props) {
 
   props.onRender?.()
 
-  const onTap = useCallback(() => {
-    'background only'
-    setAlterLogo(prevAlterLogo => !prevAlterLogo)
-  }, [])
-
   const handleInputChange = (e) => {
     setPromptInput(e.target.value)
   }
@@ -28,14 +23,17 @@ export function App(props) {
     <view>
       <view className='Background' />
       <view className='App'>
-        <view className='Banner'>
-          <view className='Logo' bindtap={onTap}>
-            {alterLogo
-              ? <image src={reactLynxLogo} className='Logo--react' />
-              : <image src={lynxLogo} className='Logo--lynx' />}
-          </view>
-          <text className='Title'>User Input is</text>
-          <text className='Subtitle'> {promptInput} </text>
+        <view className='Dashboard'>
+          <text className="Statistic">
+            <text>
+              HELLO
+            </text>
+          </text>
+          <text className="Statistic">
+            <text>
+              HELLO
+            </text>
+          </text>
         </view>
         <view className='Content'>
           <input
