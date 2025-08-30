3bc90788267f2df12fd958bf12b634d11f32ccd1
SQ77 <andrewzsq@gmail.com>
2025-08-30 11:32:44 +0800

    Improve cards layout

--- src/App.jsx
+++ src/App.jsx
@@ -15,30 +15,33 @@ export function App(props) {
     return (
         <view className="App">
             <view className="Dashboard">
-                <StatisticCard
-                    title="Post Views"
-                    statistic="82,310"
-                    changePercent={7.9}
-                    changeValue={+6_000}
-                />
-                <StatisticCard
-                    title="Profile Views"
-                    statistic="4,620"
-                    changePercent={-2.1}
-                    changeValue={-100}
-                />
-                <StatisticCard
-                    title="Likes"
-                    statistic="3,450"
-                    changePercent={+4.2}
-                    changeValue={+140}
-                />
-                <StatisticCard
-                    title="Comments"
-                    statistic="1,220"
-                    changePercent={-1.6}
-                    changeValue={-20}
-                />
+                <text className="Title">Analytics</text>
+                <view className="CardsContainer">
+                    <StatisticCard
+                        title="Post Views"
+                        statistic="82.3K"
+                        changePercent={7.9}
+                        changeValue={+6_000}
+                    />
+                    <StatisticCard
+                        title="Profile Views"
+                        statistic="4.6K"
+                        changePercent={-2.1}
+                        changeValue={-100}
+                    />
+                    <StatisticCard
+                        title="Likes"
+                        statistic="3.4K"
+                        changePercent={+4.2}
+                        changeValue={+140}
+                    />
+                    <StatisticCard
+                        title="Comments"
+                        statistic="1.2K"
+                        changePercent={-1.6}
+                        changeValue={-20}
+                    />
+                </view>
             </view>
             <view className="Content">
                 <input
