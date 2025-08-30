811790009e5920d98400e8ee887f9e255ec03a2f
SQ77 <andrewzsq@gmail.com>
2025-08-30 09:51:27 +0800

    Remove default code

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,35 @@
-import { useCallback, useEffect, useState } from '@lynx-js/react'
+import { useState } from '@lynx-js/react';
 
-import './App.css'
-import arrow from './assets/arrow.png'
-import lynxLogo from './assets/lynx-logo.png'
-import reactLynxLogo from './assets/react-logo.png'
+import './App.css';
 
 export function App(props) {
-  const [alterLogo, setAlterLogo] = useState(false)
-  const [promptInput, setPromptInput] = useState("")
+  const [promptInput, setPromptInput] = useState('');
 
-  useEffect(() => {
-    console.info('Hello, ReactLynx')
-  }, [])
-
-  props.onRender?.()
+  props.onRender?.();
 
   const handleInputChange = (e) => {
-    setPromptInput(e.target.value)
-  }
+    setPromptInput(e.target.value);
+  };
 
   return (
     <view>
-      <view className='Background' />
-      <view className='App'>
-        <view className='Dashboard'>
-          <text className="Statistic">       
-              HELLO   
-          </text>
-          <text className="Statistic">
-              HELLO
-          </text>
+      <view className="Background" />
+      <view className="App">
+        <view className="Dashboard">
+          <text className="Statistic">HELLO</text>
+          <text className="Statistic">HELLO</text>
         </view>
-        <view className='Content'>
+        <view className="Content">
           <input
             type="text"
-            className='PromptInput'
-            placeholder='Input your prompt....'
-            value={promptInput}             
-            onInput={handleInputChange}     
+            className="PromptInput"
+            placeholder="Input your prompt...."
+            value={promptInput}
+            onInput={handleInputChange}
           />
         </view>
         <view style={{ flex: 1 }} />
       </view>
     </view>
-  )
+  );
 }
