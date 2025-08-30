88d6d796d7ca6ac06d53cdf738dc3316e0cd6705
Derrick Wong <142132416+LemonDrew@users.noreply.github.com>
2025-08-30 19:35:06 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	package.json

--- src/api/backend.js
+++ src/api/backend.js
@@ -1,15 +1,49 @@
 import * as chrono from 'chrono-node';
 
 export default function parseUserinput(message) {
-    const results = [];
+    const time_metric = detectTimestamp(message);
+    const metrics = detectMetrics(message);
+
+    return {
+        timestamps: time_metric,
+        metrics: metrics
+    };
+};
+
+function detectTimestamp(message) {
     const parsedResults = chrono.parse(message);
+    const classifications = [];
+    const current_date = new Date();
 
     parsedResults.forEach(result => {
-        results.push({
-            text: result.text,
-            parsed_date: result.start.date().toISOString().replace('T', ' ').slice(0, 19)
-        });
+        const date = result.start.date();
+        const diffDays = (current_date - date) / (1000 * 60 * 60 * 24); 
+
+        const label = diffDays > 60 ? "months" : "days";
+        classifications.push({ text: result.text, label });
     });
+
+    return classifications;
+}
+
+function detectMetrics(message) {
+    const METRICS = [
+        "post views",
+        "profile views",
+        "likes",
+        "comments",
+        "shares",
+        "unique viewers"
+    ];
+
+    const results = [];
+    const lowerMessage = message.toLowerCase();
+
+    for (const metric of METRICS) {
+        if (lowerMessage.includes(metric)) {
+            results.push({ text: metric, label: "METRIC" });
+        }
+    }
 
     return results;
 }
